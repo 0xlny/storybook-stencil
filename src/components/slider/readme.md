97700d232930d66f7404e3d4f03d1905a3ef4ff3
lennycodes <lenny.aebischer>
2019-01-25 13:11:38 +0100

    Refactor slider | use webcomponents possibilities

--- src/components/slider/readme.md
+++ src/components/slider/readme.md
@@ -5,6 +5,14 @@
 <!-- Auto Generated Below -->
 
 
+## Properties
+
+| Property | Attribute | Description | Type     | Default     |
+| -------- | --------- | ----------- | -------- | ----------- |
+| `max`    | `max`     |             | `number` | `undefined` |
+| `min`    | `min`     |             | `number` | `undefined` |
+
+
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
--- src/components/slider/slider-component.tsx
+++ src/components/slider/slider-component.tsx
@@ -1,7 +1,4 @@
-import {Component, Element, Prop} from '@stencil/core';
-
-let rangeSlider;
-let rangeBullet;
+import {Component, Element, Prop, State} from '@stencil/core';
 
 @Component({
   tag: 'slider-component',
@@ -9,29 +6,42 @@ let rangeBullet;
   shadow: true
 })
 export class WelcomeComponent {
+  private rangeSlider;
+  private rangeBullet;
+
   @Prop() min: number;
   @Prop() max: number;
+  @State() value: number;
   @Element() private element: HTMLElement;
 
-  componentDidLoad() {
-    rangeSlider = this.element.shadowRoot.getElementById("rs-range-line");
-    rangeBullet = this.element.shadowRoot.getElementById("rs-bullet");
-    rangeSlider.addEventListener("input", this.showSliderValue, false);
+  constructor(){
+    this.redrawValue = this.redrawValue.bind(this);
+    this.valueChanged = this.valueChanged.bind(this);
   }
 
+  componentDidLoad() {
+    this.value = 0;
+    this.rangeSlider = this.element.shadowRoot.getElementById("rs-range-line");
+    this.rangeBullet = this.element.shadowRoot.getElementById("rs-bullet");
+    this.redrawValue();
+  }
 
-  showSliderValue() {
-    rangeBullet.innerHTML = rangeSlider.value;
-    let bulletPosition = (rangeSlider.value / this.max);
-    rangeBullet.style.left = (bulletPosition * 580) + "px";
+  valueChanged(){
+    this.value = this.rangeSlider.value;
+    this.redrawValue();
+  }
+
+  redrawValue(){
+    let bulletPosition = (this.value / this.max);
+    this.rangeBullet.style.left = (bulletPosition * 580) + "px";
   }
 
   render() {
     return (
       <div class="slider-component">
         <div class="range-slider">
-          <span id="rs-bullet" class="rs-label">0</span>
-          <input id="rs-range-line" class="rs-range" type="range" value="0" min={this.min} max={this.max}/>
+          <span id="rs-bullet" class="rs-label">{this.value}</span>
+          <input id="rs-range-line" class="rs-range" type="range" value={this.value} min={this.min} max={this.max} onInput={this.valueChanged}/>
         </div>
         <div class="box-minmax">
           <span>{this.min}</span><span>{this.max}</span>
